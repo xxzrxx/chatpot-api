fb7461366916d39376fe0ff42c098a7a4b6dc7e6
小张不学习 <1240329058@qq.com>
2024-02-26 17:42:32 +0800

    第三次：重连

--- chatpot-api-interfaces/src/test/java/org.example.chatbot.api.test/SpringBootRunTest.java
+++ chatpot-api-interfaces/src/test/java/org.example.chatbot.api.test/SpringBootRunTest.java
@@ -2,10 +2,9 @@ package org.example.chatbot.api.test;
 
 /**
  * @Auther: zhangrui
- * @Date: 2024/2/26 - 02 - 26 - 13:59
+ * @Date: 2024/2/26 - 02 - 26 - 17:25
  * @Description: org.example.chatbot.api.test
  * @version: 1.0
  */
-public class SpringBootRunTest{
+public class SpringBootRunTest {
 }
-    git pull --rebase
